3368ca41d5c803049587ea8b09c32c2a813c5d38
Ido Kadosh <1234kadosh@gmail.com>
2023-12-13 18:34:18 +0200

    chore: add app routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Models from './pages/Models'
+import Testimonials from './pages/Testimonials'
+import Team from './pages/Team'
+import Contact from './pages/Contact'
 
 const App = () => {
 	return (
 		<main className="main-app">
-			<Hero />
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />}></Route>
+					<Route path="/about" element={<About />}></Route>
+					<Route path="/models" element={<Models />}></Route>
+					<Route path="/testimonials" element={<Testimonials />}></Route>
+					<Route path="/team" element={<Team />}></Route>
+					<Route path="/contact" element={<Contact />}></Route>
+				</Routes>
+			</Router>
 		</main>
 	)
 }
